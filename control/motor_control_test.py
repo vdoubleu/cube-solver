63fce0dd9974933f1250ac24f65985389affc10d
Cindy Li <cindyli0213@hotmail.com>
2020-03-07 23:10:17 +0000

    Update motor control test

--- control/motor_control_test.py
+++ control/motor_control_test.py
@@ -2,44 +2,42 @@
 import time
 import RPi.GPIO as GPIO
 
+GPIO.setwarnings(False)
+
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
-# pin numbers
-IN1 = 17
-IN2 = 18
-IN3 = 27
-IN4 = 22
-
-pins = [IN1, IN2, IN3, IN4]
+# motor pin numbers
+motors = [[17, 18, 27, 22], # pair
+          [23, 24, 10, 9], # pair
+          [25, 11, 8, 7],
+          [5, 6, 12, 13],
+          [19, 16, 26, 20],
+          [21, 2, 3, 4]]
 
 # set up output pins
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
-GPIO.setup(IN3, GPIO.OUT)
-GPIO.setup(IN4, GPIO.OUT)
-
-GPIO.output(IN1, False)
-GPIO.output(IN2, False)
-GPIO.output(IN3, False)
-GPIO.output(IN4, False)
+for motor in motors:
+    for pin in motor:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, False)
 
 # delay between steps
-delay = 0.002
+delay = 0.0015
 
 # full step sequence
 step_count = 4
-sequence = [[1,0,0,0],
-            [0,1,0,0],
-            [0,0,1,0],
-            [0,0,0,1]]
-
+sequence = [[0,0,1,1],
+            [1,0,0,1],
+            [1,1,0,0],
+            [0,1,1,0]]
 
 def step(num_steps):
-    for i in range(num_steps):
-        for i in range(len(pins)):
-            GPIO.output(pins[i], sequence[i])
+    for step in range(num_steps):
+        for i in range(len(sequence)):
+            for motor in motors:
+                for j in range(len(motor)):
+                    GPIO.output(motor[j], sequence[i][j])
 
-        time.sleep(delay)
+            time.sleep(delay)
 
-step(10)
+step(520)
