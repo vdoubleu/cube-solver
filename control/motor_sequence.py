9889e58a8dc490748aee86e5dd1c5dbbeaf4db8e
Cindy Li <cindyli0213@hotmail.com>
2020-03-08 23:39:58 -0400

    Add rubik's cube notation to motor sequence conversion

---
+++ control/motor_sequence.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+import time
+from motor_control import rotate, MOTORS
+
+# constants
+QUARTER_TURN = 0.25
+HALF_TURN = 0.5
+LINEAR_HALF_TURN = 0.5
+
+# convert from rubik's cube move notation to motor action
+# key - notation
+# value - (motor, number of turns, clockwise)
+CONVERSION = {
+    'F': ('F', QUARTER_TURN, True),
+    'R': ('R', QUARTER_TURN, True),
+    'U': ('U', QUARTER_TURN, True),
+    'L': ('L', QUARTER_TURN, True),
+    'B': ('B', QUARTER_TURN, True),
+
+    "F'": ('F', QUARTER_TURN, False),
+    "R'": ('R', QUARTER_TURN, False),
+    "U'": ('U', QUARTER_TURN, False),
+    "L'": ('L', QUARTER_TURN, False),
+    "B'": ('B', QUARTER_TURN, False),
+
+    'F2': ('F', HALF_TURN, True),
+    'R2': ('R', HALF_TURN, True),
+    'U2': ('U', HALF_TURN, True),
+    'L2': ('L', HALF_TURN, True),
+    'B2': ('B', HALF_TURN, True),
+    'D2': ('D', HALF_TURN, True)
+}
+
+# converts a series of notations to a motor sequence and executes each action
+def execute_sequence(notations):
+
+    for notation in notations.split():
+        action = CONVERSION[notation]
+
+        if action[0] == 'F':
+            rotate(action[1], MOTORS['F-rotation']=action[2])
+            if action[1] == QUARTER_TURN:
+                rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+                rotate(action[1], MOTORS['F-rotation']=!action[2])
+                rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+        
+        elif action[0] == 'R':
+            rotate(action[1], MOTORS['R-rotation']=action[2])
+            if action[1] == QUARTER_TURN:
+                rotate(LINEAR_HALF_TURN, MOTORS['RL-linear']=True)
+                rotate(action[1], MOTORS['R-rotation']=!action[2])
+                rotate(LINEAR_HALF_TURN, MOTORS['RL-linear']=True)
+        
+        elif action[0] == 'L':
+            rotate(action[1], MOTORS['L-rotation']=action[2])
+            if action[1] == QUARTER_TURN:
+                rotate(LINEAR_HALF_TURN, MOTORS['RL-linear']=True)
+                rotate(action[1], MOTORS['L-rotation']=!action[2])
+                rotate(LINEAR_HALF_TURN, MOTORS['RL-linear']=True)
+                
+        elif action[0] == 'B':
+            rotate(action[1], MOTORS['B-rotation']=action[2])
+            if action[1] == QUARTER_TURN:
+                rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+                rotate(action[1], MOTORS['B-rotation']=!action[2])
+                rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+
+        elif action[0] == 'U':
+            rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+            rotate(QUARTER_TURN, MOTORS['L-rotation']=True, MOTORS['R-rotation']=False)
+            rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+            
+            rotate(LINEAR_HALF_TURN, MOTORS['LR-linear']=True)
+            rotate(QUARTER_TURN, MOTORS['L-rotation']=False, MOTORS['R-rotation']=True)
+            rotate(LINEAR_HALF_TURN, MOTORS['LR-linear']=True)
+
+            rotate(action[1], MOTORS['F-rotation']=action[2])
+            rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+
+            if action[1] == QUARTER_TURN:
+                rotate(action[1], MOTORS['L-rotation']=True, MOTORS['R-rotation']=False, MOTORS['F-rotation']=!action[2])
+            else:
+                rotate(LINEAR_HALF_TURN, MOTORS['L-rotation']=True, MOTORS['R-rotation']=False)
+
+            rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+
+            rotate(LINEAR_HALF_TURN, MOTORS['LR-linear']=True)
+            rotate(QUARTER_TURN, MOTORS['L-rotation']=False, MOTORS['R-rotation']=True)
+            rotate(LINEAR_HALF_TURN, MOTORS['LR-linear']=True)
+
+        elif action[0] == 'D':
+            rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+            rotate(QUARTER_TURN, MOTORS['L-rotation']=True, MOTORS['R-rotation']=False)
+            rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+            
+            rotate(LINEAR_HALF_TURN, MOTORS['LR-linear']=True)
+            rotate(QUARTER_TURN, MOTORS['L-rotation']=False, MOTORS['R-rotation']=True)
+            rotate(LINEAR_HALF_TURN, MOTORS['LR-linear']=True)
+
+            rotate(action[1], MOTORS['B-rotation']=action[2])
+            rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+
+            if action[1] == QUARTER_TURN:
+                rotate(action[1], MOTORS['L-rotation']=True, MOTORS['R-rotation']=False, MOTORS['B-rotation']=!action[2])
+            else:
+                rotate(LINEAR_HALF_TURN, MOTORS['L-rotation']=True, MOTORS['R-rotation']=False)
+
+            rotate(LINEAR_HALF_TURN, MOTORS['FB-linear']=True)
+
+            rotate(LINEAR_HALF_TURN, MOTORS['LR-linear']=True)
+            rotate(QUARTER_TURN, MOTORS['L-rotation']=False, MOTORS['R-rotation']=True)
+            rotate(LINEAR_HALF_TURN, MOTORS['LR-linear']=True)
---
+++ control/stepper_control.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import time
+import RPi.GPIO as GPIO
+
+# motor pin numbers 
+# key - identifier
+# value - (IN1, IN2, IN3, IN4)
+MOTORS = {'LR-linear': (17, 18, 27, 22), # pair
+          'FB-linear': (23, 24, 10, 9),  # pair
+          'F-rotation': (25, 11, 8, 7),
+          'B-rotation': (5, 6, 12, 13),
+          'L-rotation': (19, 16, 26, 20),
+          'R-rotation': (21, 2, 3, 4)}
+
+# constants
+STEPS_PER_REV = 520
+DELAY = 0.0015
+RANGES = [[0, 1, 2, 3], [3, 2, 1, 0]]
+
+# full step sequence
+SEQUENCE = [[0,0,1,1],
+            [1,0,0,1],
+            [1,1,0,0],
+            [0,1,1,0]]
+
+def setup():
+    GPIO.setwarnings(False)
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+
+    for motor in MOTORS.values():
+        for pin in motor:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, False)
+
+# rotate motor
+def rotate(num_rev, **motors):
+    num_steps = num_rev * STEPS_PER_REV
+
+    for step in range(num_steps):
+
+        for i in range(len(SEQUENCE)):
+            # direction: 0 = ccw, 1 = cw
+            for motor, direction in motors:
+                for j in range(len(motor)):
+                    GPIO.output(motor[j], SEQUENCE[RANGES[direction][i]][j])
+
+            time.sleep(DELAY)
+
+
+if __name__ == '__main__':
+    setup()
+    rotate_cw(1, motor=True for motor in MOTORS.values()) # one rotation
